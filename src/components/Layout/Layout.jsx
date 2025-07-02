e5fa291b6da7b0787bd9df4b76575c9483863479
Balaganesh1122 <ganeshgolla2019@gmail.com>
2025-07-02 11:01:42 +0530

    layout

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Sidebar from './Sidebar';
+
+export default function Layout() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header onMenuToggle={() => setSidebarOpen(!sidebarOpen)} />
+      
+      <div className="flex">
+        <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+        
+        <main className="flex-1 lg:ml-0">
+          <div className="p-4 lg:p-6">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
